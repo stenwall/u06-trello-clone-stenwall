5163d81a342d50c93b7af915e528590aeaf28735
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-10 21:34:09 +0100

    add dialog

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
-$(()=>{
-	$("#todo-list-body, #doing-list-body, #done-list-body")
+const $dialog = $("#dialog");
+const $editBtn = $(".edit-btn");
+
+$(() => {
+  $("#todo-list-body, #doing-list-body, #done-list-body")
     .sortable({
       connectWith: ".list-body",
-	  cursor: "grabbing"
+      cursor: "grabbing",
+      sort: function (event, ui) {},
     })
     .disableSelection();
+
+  $dialog.dialog({
+    autoOpen: false,
+    resizable: false,
+    modal: true,
+    show: {
+      effect: "fold",
+      duration: 400,
+      size: 50,
+    },
+    hide: {
+      effect: "fold",
+      duration: 400,
+      size: 50,
+    },
+    buttons: [
+      {
+        text: "Save",
+        class: "btn btn-sm btn-secondary",
+        click: () => {
+          $dialog.dialog("close");
+        },
+      },
+    ],
+  });
+
+  $editBtn.on("click", () => {
+    $dialog.dialog("open");
+  });
+  
 });
