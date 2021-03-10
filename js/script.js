15c965ef4159852a9f0eb7bb8b14a38fbddeb81d
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-10 18:27:14 +0100

    add cursor effect

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 $(()=>{
 	$("#todo-list-body, #doing-list-body, #done-list-body")
     .sortable({
-		connectWith: ".list-body"
-	})
+      connectWith: ".list-body",
+	  cursor: "grabbing"
+    })
     .disableSelection();
 });
