ddaebe7f28173707eb9e2bbd329146195bd31f8b
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-11 16:26:56 +0100

    test w classes in widget

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const $dialog = $("#dialog");
 const $editBtn = $(".edit-btn");
+const $tabs = $("#tabs");
 
-$(() => {
+$(function () {
   $("#todo-list-body, #doing-list-body, #done-list-body")
     .sortable({
       connectWith: ".list-body",
@@ -12,31 +13,64 @@ $(() => {
 
   $dialog.dialog({
     autoOpen: false,
+    classes: {
+      "ui-dialog": "modal-content",
+      "ui-dialog-titlebar": "modal-header",
+      "ui-dialog-title": "modal-title",
+      "ui-dialog-titlebar-close": "close",
+      "ui-dialog-content": "modal-body",
+      "ui-dialog-buttonpane": "modal-footer",
+    },
     resizable: false,
     modal: true,
+    height: "auto",
+    width: "60vw",
+	title: "Edit todo",
     show: {
-      effect: "fold",
+      effect: "fade",
       duration: 400,
       size: 50,
     },
     hide: {
-      effect: "fold",
+      effect: "fade",
       duration: 400,
-      size: 50,
+      size: 100,
     },
-    buttons: [
-      {
-        text: "Save",
-        class: "btn btn-sm btn-secondary",
-        click: () => {
-          $dialog.dialog("close");
-        },
+    buttons: {
+      Save: function () {
+        $(this).dialog("close");
       },
-    ],
+      Cancel: function () {
+        $dialog.dialog("close");
+      },
+    },
   });
+  // .extend($.ui.dialog.prototype.options.classes, {
+  //   "ui-dialog": "modal-content",
+  //   "ui-dialog-titlebar": "modal-header",
+  //   "ui-dialog-title": "modal-title",
+  //   "ui-dialog-titlebar-close": "close",
+  //   "ui-dialog-content": "modal-body",
+  //   "ui-dialog-buttonpane": "modal-footer",
+  // });
+
+  $tabs.tabs().removeClass("ui-tabs-nav").addClass("nav nav-tabs");
+
+  //   $tabs.tabs({
+  //     classes: {
+  //       "ui-tabs-nav": "nav nav-tabs",
+  //       "ui-tabs-panel": "tab-pane",
+  //       "ui-tabs-active": "active",
+  //     },
+  //   });
+
+  //   $tabs.extend($.ui.tabs.prototype.options.classes, {
+  //     "ui-tabs-nav": "nav nav-tabs",
+  //     "ui-tabs-panel": "tab-pane",
+  //     "ui-tabs-active": "active",
+  //   });
 
   $editBtn.on("click", () => {
     $dialog.dialog("open");
   });
-  
 });
