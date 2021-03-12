857465ab9fe611fc8b3580aa5016b68c64063fe6
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-12 12:10:57 +0100

    lite tok

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const $dialog = $("#dialog");
 const $editBtn = $(".edit-btn");
 const $tabs = $("#tabs");
+const $datepicker = $("#datepicker");
+const $cardFooters = $(".card-footer");
 
 $(function () {
   $("#todo-list-body, #doing-list-body, #done-list-body")
@@ -11,8 +13,9 @@ $(function () {
     })
     .disableSelection();
 
-  $dialog.dialog({
-    autoOpen: false,
+  $cardFooters.hide();
+
+  $.widget("ui.bootstrapDialog", $.ui.dialog, {
     classes: {
       "ui-dialog": "modal-content",
       "ui-dialog-titlebar": "modal-header",
@@ -21,30 +24,84 @@ $(function () {
       "ui-dialog-content": "modal-body",
       "ui-dialog-buttonpane": "modal-footer",
     },
+  });
+
+  $.ui.bootstrapDialog.prototype.options.classes,
+    {
+      "ui-dialog": "modal-content",
+      "ui-dialog-titlebar": "modal-header",
+      "ui-dialog-title": "modal-title",
+      "ui-dialog-titlebar-close": "close",
+      "ui-dialog-content": "modal-body",
+      "ui-dialog-buttonpane": "modal-footer",
+    };
+
+  $dialog.bootstrapDialog({
+    autoOpen: false,
     resizable: false,
     modal: true,
     height: "auto",
     width: "60vw",
-	title: "Edit todo",
+    title: "Edit todo",
     show: {
       effect: "fade",
       duration: 400,
       size: 50,
     },
     hide: {
-      effect: "fade",
-      duration: 400,
-      size: 100,
+      effect: "puff",
+      duration: 500,
+      size: 50,
     },
-    buttons: {
-      Save: function () {
-        $(this).dialog("close");
+    buttons: [
+      {
+        text: "Save",
+        class: "btn btn-sm btn-secondary",
+        click: function () {
+          $dialog.bootstrapDialog("close").removeClass(".ui-button");
+        },
+        create: function () {
+          $dialog
+            .bootstrapDialog()
+            .find(".ui-button")
+            .removeClass(".ui-button");
+        },
       },
-      Cancel: function () {
-        $dialog.dialog("close");
-      },
+    ],
+    // buttons: {
+    //   Save: function () {
+    //     $(this).bootstrapDialog("close");
+    //   },
+    //   Cancel: function () {
+    //     $dialog
+    //       .bootstrapDialog("close")
+    //       .find(".ui-button")
+    //       .addClass("btn btn-sm btn-secondary");
+    //   },
+    //   1: {
+    //     id: "close",
+    //     text: "Close",
+    //     click: function () {
+    //       $(this).bootstrapDialog("close").addClass("btn btn-sm btn-secondary");
+    //     },
+    //     class: "btn btn-sm",
+    //   },
+    // },
+    open: function () {
+      $datepicker.datepicker({
+        minDate: +1,
+        dateFormat: "d/m, -y",
+        firstDay: 1,
+        showAnim: "blind",
+        altField: ".deadline-date",
+        altFormat: "d/m, -y",
+        onSelect: function (altFormat) {
+          $cardFooters.show();
+        },
+      });
     },
   });
+
   // .extend($.ui.dialog.prototype.options.classes, {
   //   "ui-dialog": "modal-content",
   //   "ui-dialog-titlebar": "modal-header",
@@ -55,6 +112,10 @@ $(function () {
   // });
 
   $tabs.tabs().removeClass("ui-tabs-nav").addClass("nav nav-tabs");
+
+  $dialog.removeClass(
+    "ui-widget-content ui-widget ui-widget-header ui-dialog ui-dialog-title ui-dialog-content ui-dialog-buttonpane"
+  );
 
   //   $tabs.tabs({
   //     classes: {
@@ -71,6 +132,6 @@ $(function () {
   //   });
 
   $editBtn.on("click", () => {
-    $dialog.dialog("open");
+    $dialog.bootstrapDialog("open");
   });
 });
